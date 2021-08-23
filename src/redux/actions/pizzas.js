d039c723ba2411a2391d454349e3612c89ddf9fa
Evgenii Lapotko <lapotko.e2@gmail.com>
2021-08-23 11:46:54 +0300

    added filter and sorting

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -5,9 +5,11 @@ export const setLoaded = (val) => ({
 
 export const fetchPizzas = (category, sortBy) => async (dispatch) => {
     dispatch(setLoaded(false));
-
+    console.log(category, sortBy);
     const resolve = await fetch(
-        `https://612299d2d4462800170548e6.mockapi.io/pizzas`
+        `https://612299d2d4462800170548e6.mockapi.io/pizzas?category=${
+            category === null ? "" : category
+        }&sortby=${sortBy}&order=desc`
     );
 
     const data = await resolve.json();
